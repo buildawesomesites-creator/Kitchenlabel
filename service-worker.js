0d628d76a41c47b68813d5cfbe64cb26d445e459
buildawesomesites-creator <buildawesomesites@gmail.com>
2025-10-07 12:06:59 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
-self.addEventListener("install", (e) => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open("kitchen-label-cache").then((cache) => {
+    caches.open('kitchen-v1').then((cache) => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-512.png',
+        './icon-192.png'
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
   );
